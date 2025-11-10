2b6b07e2a1caa3e7c7d46c5c3361f0923c1a167f
Davies Wabuluka <dwabuluka@gmail.com>
2025-11-10 09:28:48 -0600

    backend: crud for products with image upload

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { signin, signout, signup } from "../controllers/auth.controller.js";
+import {
+  signin,
+  signout,
+  signup,
+  refreshToken,
+  getProfile,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
+router.post("/refresh-token", refreshToken);
+router.get("/profile", getProfile);
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import expressWinston from "express-winston";
-import {
-  errorHandler,
-  AppError,
-} from "./middleware/errorHandler.middleware.js";
 
-import authRouter from "./routes/auth.route.js";
 import { ENV } from "./configs/env.js";
 import { connectDB } from "./configs/db.js";
 import { logger } from "./utils/logger.js";
+
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 const app = express();
 app.use(express.json());
@@ -41,7 +39,8 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK", timestamp: new Date().toISOString() });
 });
 
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 const PORT = ENV.PORT;
 app.listen(PORT, () => {
