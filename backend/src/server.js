6e62b11ddbce93c077611a3fc8d9555243b920e4
Davies Wabuluka <dwabuluka@gmail.com>
2025-11-10 17:57:08 -0600

    all the rest of backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,10 @@ import { logger } from "./utils/logger.js";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticRoutes from "./routes/analytics.route.js";
 
 const app = express();
 app.use(express.json());
@@ -41,8 +45,13 @@ app.get("/health", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticRoutes);
 
 const PORT = ENV.PORT;
+
 app.listen(PORT, () => {
   console.log(`Backend Server running on http://localhost:${PORT}`);
   connectDB();
